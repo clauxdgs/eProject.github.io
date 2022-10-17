ce972b6bac337c5a6bce1cd4e8d9bddd9f18110c
ClauXDgs <clau.guesan@gmail.com>
2022-10-17 16:00:46 -0300

    trashicon

--- js/cart.js
+++ js/cart.js
@@ -44,7 +44,12 @@ function showCart(array) {
                     <td class="col-2">
                     <input id="cantidad${element.id}" class="quantity cant col-5 text-center" min="1" max="999999" name="quantity" value="${element.count}" type="number" onchange="calcSubtotal(${element.id},${element.unitCost})"> </td>
                     <td class="col-2"><strong> ${element.currency} <label id="subtotal${element.id}">${element.unitCost*element.count} </label> </strong></td>
-                    <td class="col-2"> Eliminar </td>
+                    <td class="col-2">
+                    <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewbox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                    </svg> 
+                    </button> 
+                    </td>
                </tr>
                </tbody>
           </table>`
